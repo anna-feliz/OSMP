b2faefa6631fd8fd8b2eb04caa759e311048acb2
philip-akerfeldt <Philip.akerfeldt@hotmail.com>
2014-03-12 12:57:55 +0100

    added some description to the splitList function, like psuedo code

--- miniProjekt/splitList.erl
+++ miniProjekt/splitList.erl
@@ -3,14 +3,28 @@
 -module(splitList).
 
 
+
+%% To use EUnit we must include this library.
+-include_lib("eunit/include/eunit.hrl").
+
 %% TODO:
-%% 
+%% använd split(N, List1) -> list2 (N första elementen) + list3 (tail) 
+%% anropa split två gånger
 
-%% @doc Vad funktionen gör
--spec splitList() -> 
-
-splitList(A, B, N) ->
+%% @doc Will split the lists A and B in N equal long parts.
+-spec splitList() -> [{}] when 
+      
+splitList(A, B, N) -> 
+    %% Kolla om listan A är mindre än N
+    %% om nej --> anropa lists:split(N, A) 
     
+
+
+
+
+
+
+
 
 
 
