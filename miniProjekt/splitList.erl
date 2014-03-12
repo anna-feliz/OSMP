9ffd307722a55f3215631b2a920ae4a350c95d9f
philip-akerfeldt <Philip.akerfeldt@hotmail.com>
2014-03-12 11:42:58 +0100

    Added the file for the splitlist function

---
+++ miniProjekt/splitList.erl
@@ -0,0 +1,31 @@
+%% @author Felix Färsjö, Jimmy Holm, Fredrik Larsson, Anna Nilsson, Philip Åkerfeldt
+
+-module(splitList).
+
+
+%% TODO:
+%% 
+
+%% @doc Vad funktionen gör
+-spec splitList() -> 
+
+splitList(A, B, N) ->
+    
+
+
+
+
+
+
+
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%                         EUnit Test Cases                                  %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% EUnit adds the splitlist() function to this module. 
+
+start_test() ->
+    ?assertMatch(true, is_pid(start(10))).
